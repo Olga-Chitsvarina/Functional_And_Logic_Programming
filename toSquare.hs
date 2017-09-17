99d39e5b3359e3b4e9dbe43271c4a1278a42362f
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-09-16 20:59:14 -0600

    Add files via upload

---
+++ toSquare.hs
@@ -0,0 +1,4 @@
+--This function takes the integer input (parameter) and 
+--returns the square of that number as the output (integer).
+toSquare :: Int -> Int 	-- Parameter type: integer. Output type: integer
+toSquare x = x*x	-- Square the number and return it
