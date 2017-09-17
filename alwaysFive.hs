6abdf199a08b1d4aee591b968e5b873f85b5566a
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-09-16 20:03:45 -0600

    Add files via upload

---
+++ alwaysFive.hs
@@ -0,0 +1,4 @@
+--This function always returns the number 5
+alwaysFive :: Int 	-- Define the name of the function ("alawaysFive")
+			-- Specify that "alwaysFive" returns an Integer
+alwaysFive = 5		-- Return the integer 5 as the output for the function
