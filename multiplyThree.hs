f430b00a7be6d5063e11f271af9aaf3a571a3029
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-10-09 11:57:31 -0600

    Add files via upload

---
+++ multiplyThree.hs
@@ -0,0 +1,6 @@
+--Author: Olga Chitsvarina
+--Date: October 9th, 2017 
+
+-- This function multiplies 3 Float numbers:
+multiplyThree :: Float -> Float -> Float -> Float
+multiplyThree a b c = a*b*c
