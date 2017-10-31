8025e0113f50c5fd83fa5e3c7509d3b8730923a6
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-10-31 17:28:25 -0600

    Add files via upload

---
+++ convertToDogYears.hs
@@ -0,0 +1,16 @@
+--Name: Olga Chitsvarina
+--Date: October 31, 2017
+-----------------------------------------------------------------------------
+-- This method takes the float number as a parameter (human age) and returns
+-- the floating point number, representing the dog years as its result.
+convertToDogYears :: Float -> Float 
+convertToDogYears 0 = 0
+convertToDogYears age 
+        | (abs age) <= 2 = ((abs age) * 10.5) * sign
+        | otherwise = (2 * 10.5 + rest*4) * sign
+        
+
+ where 
+        rest = (abs age) - 2
+        sign = age/(abs age)
+ 
