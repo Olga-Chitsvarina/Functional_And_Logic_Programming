a794d2c5dc4e531fabe52e0ff88adf5fa83c061b
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-10-31 18:52:43 -0600

    Add files via upload

--- notDivBy5And7List.hs
+++ notDivBy5And7List.hs
@@ -5,24 +5,32 @@
 -- they remove the integers, which are divisible by 5 or 7 (or both)
 -- and return the resulting list of integers:
 
-returnNotDivBy5And7List :: [Int] -> [Int]
-returnNotDivBy5And7List [] = []
-returnNotDivBy5And7List list
-        | (myHead `mod` 5 == 0) = returnNotDivBy5And7List myTail
-        | (myHead `mod` 7 == 0) = returnNotDivBy5And7List myTail
-        | otherwise = myHead : returnNotDivBy5And7List myTail
+returnNotDivBy5And7ListRec1 :: [Int] -> [Int]
+returnNotDivBy5And7ListRec1 [] = []
+returnNotDivBy5And7ListRec1 list
+        | (myHead `mod` 5 == 0) = returnNotDivBy5And7ListRec1 myTail
+        | (myHead `mod` 7 == 0) = returnNotDivBy5And7ListRec1 myTail
+        | otherwise = myHead : returnNotDivBy5And7ListRec1 myTail
         
        where 
        myHead = head list
        myTail = tail list
 
 
-returnNotDivBy5And7List' :: [Int] -> [Int]
-returnNotDivBy5And7List' [] = []
-returnNotDivBy5And7List' (x:xs)
-        | (x `mod` 5 == 0) = returnNotDivBy5And7List' xs
-        | (x `mod` 7 == 0) = returnNotDivBy5And7List' xs
-        | otherwise = x : returnNotDivBy5And7List' xs
+returnNotDivBy5And7ListRec2 :: [Int] -> [Int]
+returnNotDivBy5And7ListRec2 [] = []
+returnNotDivBy5And7ListRec2 (x:xs)
+        | (x `mod` 5 == 0) = returnNotDivBy5And7ListRec2 xs
+        | (x `mod` 7 == 0) = returnNotDivBy5And7ListRec2 xs
+        | otherwise = x : returnNotDivBy5And7ListRec2 xs
+
+
+returnNotDivBy5And7ListComprehensions :: [Int] -> [Int]
+returnNotDivBy5And7ListComprehensions list =    [x|x <- list, 
+                                                (x `rem` 5)/= 0, 
+                                                (x `rem` 7)/= 0]
         
+
+
       
         
