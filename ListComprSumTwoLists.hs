78aa76686fb1d4d57a3f6960a87dafe4aeb2877d
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-10-31 19:09:13 -0600

    Add files via upload

---
+++ ListComprSumTwoLists.hs
@@ -0,0 +1,12 @@
+--Name: Olga Chitsvarina
+--Date: October 31, 2017
+-----------------------------------------------------------------------------
+
+sumLists :: [Int] -> [Int] -> [Int]
+sumLists listA listB = [a + b | a <- listA, b<- listB]
+
+        
+
+
+      
+        
