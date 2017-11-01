73ecbbfbee0f3ba8396b6b7d499218e25f580b72
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-10-31 23:13:41 -0600

    Add files via upload

--- PolymorphicFunctions.hs
+++ PolymorphicFunctions.hs
@@ -1,10 +1,13 @@
 --Name: Olga Chitsvarina
 --Date: October 31, 2017
 -----------------------------------------------------------------------------
-
-addTwo :: Int -> Int
+-- This is a polymorphic function, which takes the Num parameter A, computes A+2
+-- and returns it
+addTwo :: Num a => a -> a
 addTwo a1 = a1 + 2
 
+-- This is a polymorphic function, which takes a function and a tuple as parameters
+-- and applies the function to every element in the tuple, it returns the resulting tuple
 applyToTuple :: (a -> b) -> (a,a) -> (b,b)
 applyToTuple f (a1,a2) = (f a1, f a2)
 
