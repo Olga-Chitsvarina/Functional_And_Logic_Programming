c5636f5ed14e24c5ca32549288d6a25669c7d1c7
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 14:19:13 -0600

    Delete multiplyThree.hs

--- multiplyThree.hs
+++
@@ -1,6 +0,0 @@
---Author: Olga Chitsvarina
---Date: October 9th, 2017 
-
--- This function multiplies 3 Float numbers:
-multiplyThree :: Float -> Float -> Float -> Float
-multiplyThree a b c = a*b*c
