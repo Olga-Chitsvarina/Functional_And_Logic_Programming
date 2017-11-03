17272492ee820584b79ff017a6a80cbb73bb1948
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 14:24:52 -0600

    Delete sumArray.hs

--- sumArray.hs
+++
@@ -1,9 +0,0 @@
---Author: Olga Chitsvarina
---Date: October 9th, 2017 
-
--------------------------------------------------------------------
---This function takes the array of integers and
--- it returns their sum
-sumArray :: [Int] -> Int
-sumArray [] = 0
-sumArray (x : xs) = x + sumArray xs
