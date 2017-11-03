f9e8172b317d5f7bb1c28e9148125fcb0e650753
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 14:44:50 -0600

    Delete removeEven.hs

--- removeEven.hs
+++
@@ -1,8 +0,0 @@
---Author: Olga Chitsvarina
---Date: October 9th, 2017 
-
--------------------------------------------------------------------
--- This function takes the array of integers, it removes the 
--- even values and returns only the odd ones.
-removeEven :: [Int] -> [Int]
-removeEven integers = [x| x <- integers, x `rem` 2 == 1]
