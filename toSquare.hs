39dacfdca6b82e04947d1e001ad9137bd5023963
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 14:08:18 -0600

    Delete toSquare.hs

--- toSquare.hs
+++
@@ -1,4 +0,0 @@
---This function takes the integer input (parameter) and 
---returns the square of that number as the output (integer).
-toSquare :: Int -> Int 	-- Parameter type: integer. Output type: integer
-toSquare x = x*x	-- Square the number and return the result
