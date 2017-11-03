43096728aabf74509d56f6ebfa8b332eb7b1e2a3
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 15:15:10 -0600

    Add files via upload

--- Simple.hs
+++ Simple.hs
@@ -215,3 +215,20 @@ howManyAreEqual x y z
     | threeAreNotEqual x y z == True = 0
     | otherwise                      = 2
 ---------------------------------------------------------------
+
+-- This function takes the integer parameter 
+-- It return true if that parameter is an even number
+-- and False otherwise
+isEven :: Int -> Bool
+isEven x
+    | x `rem` 2 == 0 = True
+    | otherwise = False
+
+
+-- This function takes the integer parameter 
+-- It return true if that parameter is a odd number
+-- and False otherwise
+isOdd :: Int -> Bool
+isOdd x
+    | isEven x == True = False
+    | otherwise = True
