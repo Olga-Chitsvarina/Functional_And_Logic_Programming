6a944fff97c5ab26ff2273bd8e43598469bcf413
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 14:44:25 -0600

    Update ListComprehensions.hs

--- ListComprehensions.hs
+++ ListComprehensions.hs
@@ -5,6 +5,8 @@
 sumLists :: [Int] -> [Int] -> [Int]
 sumLists listA listB = [a + b | a <- listA, b<- listB]
 
+------------------------------------------------------------------------------
+
 -- Sum first element with the first element of the list... nth element of the 
 -- first list with the nth element of the second list:
 sumListItems :: [Int] -> [Int] -> [Int]
@@ -13,6 +15,11 @@ sumListItems listA listB = [fst element + snd element | element <- zippedList]
         where
         zippedList = zip listA listB
         
+------------------------------------------------------------------------------
+-- This function takes the array of integers, it removes the 
+-- even values and returns only the odd ones.
+removeEven :: [Int] -> [Int]
+removeEven integers = [x| x <- integers, x `rem` 2 == 1]
 
         
 
