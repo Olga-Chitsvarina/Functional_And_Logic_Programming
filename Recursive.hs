8a2f75b704dd2670562cffc031fb047c6244f7a9
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2017-11-03 14:30:28 -0600

    Add files via upload

---
+++ Recursive.hs
@@ -0,0 +1,29 @@
+
+----------------------------------------------------------------
+--This function takes the array of integers and
+-- it returns their sum
+sumArray :: [Int] -> Int
+sumArray [] = 0
+sumArray (x : xs) = x + sumArray xs
+
+--------------------------------------------------------------
+-- This function takes one parameter of type Int
+-- It returns the factorial value of that number
+factorial :: Int -> Int
+factorial x
+    | x < 0 = error "Number should be a nonnegative integer"
+    | x == 0 = 1
+    | x == 1 = 1
+    | otherwise = x* (factorial(x-1))
+
+----------------------------------------------------------
+-- This function takes the Int number as a parameter and
+-- it returns the n'th Fibonacci number as output
+fib :: Int -> Int
+fib n
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = fib(n -1) + fib (n-2
+
+--------------------------------------------------------------
+
--- Simple.hs
+++ Simple.hs
@@ -160,3 +160,58 @@ convertToDogYears age
  where 
         rest = (abs age) - 2
         sign = age/(abs age)
+
+----------------------------------------------------------------------------
+
+--This function divides the floating point numerator 
+-- by the floating point denominator. 
+-- This function signals about the error, if denominator is 0.
+-- Pattern matching is used here:
+myDiv :: Float -> Float -> Float
+myDiv _ 0 = error "\nERROR :(\nYou cannot divide by 0!!!"
+myDiv numerator denominator = numerator / denominator
+
+
+--This function divides the floating point numerator 
+-- by the floating point denominator. 
+-- This function signals about the error, if denominator is 0.
+-- Guards are used here:
+myDiv2 :: Float -> Float -> Float
+myDiv2 numerator denominator 
+  | denominator /= 0 = numerator /denominator
+  | otherwise = error "\nERROR :(\nYou cannot divide by 0!!!"
+
+---------------------------------------------------------------------
+
+-- This function takes 2 Floats
+-- This function returns 
+-- True if two numbers are equal
+-- Otherwise, it returns False:
+twoAreEqual :: Float -> Float -> Bool
+twoAreEqual x y = x == y
+
+--This function takes 3 Floats
+--This function returns
+--True if three numbers are equal
+--Otherwise, it returns False:
+threeAreEqual :: Float -> Float -> Float -> Bool
+threeAreEqual x y z = (twoAreEqual x y) && (twoAreEqual y z)
+
+-- This function takes 3 Floats
+-- This function returns
+-- True, if none of three numbers are equal to the other one
+-- False, if at least one pair of equal numbers is present
+threeAreNotEqual :: Float -> Float -> Float -> Bool
+threeAreNotEqual x y z = (x/=y)&&(y/=z)&&(z/=x)
+
+--This function takes three Floats
+-- This function returns
+-- 3 if all three numbers are equal
+-- 2 if two numbers are equal
+-- 0 if there is no equal pairs of numbers
+howManyAreEqual :: Float -> Float -> Float -> Int
+howManyAreEqual x y z 
+    | threeAreEqual x y z    == True = 3
+    | threeAreNotEqual x y z == True = 0
+    | otherwise                      = 2
+---------------------------------------------------------------
